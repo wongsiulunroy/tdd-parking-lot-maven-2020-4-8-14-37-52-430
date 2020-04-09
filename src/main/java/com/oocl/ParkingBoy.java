a9d370d8b069f418fd4aca2cab92af773f0a2599
wongro2 <roy.wong@oocl.com>
2020-04-09 15:44:40 +0800

    [Roy] created parkingBoy_should_fetch_a_car_with_a_parkingTicket testcase

--- src/main/java/com/oocl/ParkingBoy.java
+++ src/main/java/com/oocl/ParkingBoy.java
@@ -1,4 +1,10 @@
 package com.oocl;
 
 public class ParkingBoy {
+    ParkingLot parkingLot = new ParkingLot(10);
+
+    public ParkingTicket park(Car car) {
+        return this.parkingLot.park(car);
+    }
+
 }
--- src/test/java/com/oocl/ParkingBoyTest.java
+++ src/test/java/com/oocl/ParkingBoyTest.java
@@ -1,4 +1,23 @@
 package com.oocl;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingBoyTest {
+
+    @Test
+    public void parkingBoy_should_park_a_car(){
+        ParkingBoy parkingBoy = new ParkingBoy();
+        ParkingTicket parkingTicket = parkingBoy.park(new Car());
+        Assert.assertNotNull(parkingTicket);
+    }
+    @Test
+    public void parkingBoy_should_fetch_a_car_with_a_parkingTicket(){
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car = new Car();
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+        Car fetchedCar = parkingBoy.fetch(parkingTicket);
+        Assert.assertEquals(car, fetchedCar);
+    }
+
 }
