673475809251372c4cb93c11f09c28ad0d3838e0
wongro2 <roy.wong@oocl.com>
2020-04-14 17:10:00 +0800

    [Roy] updated park method in ParkingBoy

--- src/main/java/com/oocl/ParkingBoy.java
+++ src/main/java/com/oocl/ParkingBoy.java
@@ -13,22 +13,22 @@ public class ParkingBoy {
     }
 
     public ParkingTicket park(Car car) {
-        ParkingLot selectedParking = this.parkingLots.stream().filter(parkingLot-> !parkingLot.isFull()).findFirst().get();
+        ParkingLot selectedParking = this.parkingLots.stream()
+                .filter(parkingLot-> !parkingLot.isFull())
+                .findFirst()
+                .orElseThrow(ParkingLotFullException::new);
         return selectedParking.park(car);
     }
 
     public Car fetch(ParkingTicket parkingTicket)  {
-        //if (parkingTicket ==null){
-        //    throw new MissingParkingTicket();
-        //}
         return this.parkingLots.get(0).fetch(parkingTicket);
     }
 
     public ParkingLot getParkingLot() {
         return parkingLot;
     }
-
+    /*
     public List<ParkingLot> getParkingLots() {
         return parkingLots;
-    }
+    }*/
 }
--- src/main/java/com/oocl/ParkingLot.java
+++ src/main/java/com/oocl/ParkingLot.java
@@ -5,32 +5,26 @@ import java.util.Map;
 
 
 public class ParkingLot {
-
-
-    private final int capacity;
+    private int capacity;
     private Map<ParkingTicket, Car> parkingTicketMap = new HashMap();
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
     }
-
-    public ParkingLot(){
-        this.capacity =10;
+    public ParkingLot() {
+        this.capacity = 10;
     }
 
     public ParkingTicket park(Car car) {
-        if(isFull()){
+        if (isFull()) {
             throw new ParkingLotFullException();
         }
-        if (car == null){
+        if (car == null || parkingTicketMap.containsValue(car) ) {
             return null;
         }
-        if(parkingTicketMap.containsValue(car)){
-            return null;
-        }
-
         ParkingTicket parkingTicket = new ParkingTicket();
         this.parkingTicketMap.put(parkingTicket, car);
+        //this.capacity -= 1 ;
         return parkingTicket;
     }
 
@@ -42,10 +36,9 @@ public class ParkingLot {
         if (parkingTicket == null) {
             throw new MissingParkingTicket();
         }
-        if (!this.parkingTicketMap.containsKey(parkingTicket)){
+        if (!this.parkingTicketMap.containsKey(parkingTicket)) {
             throw new UnrecognisedParkingTicket();
         }
-
         Car car = this.parkingTicketMap.remove(parkingTicket);
         return car;
     }
